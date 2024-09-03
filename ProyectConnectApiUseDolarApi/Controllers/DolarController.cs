02832bb9346a1ba792765c8287f3b1be686fd49f
Ursula <Ursula@DESKTOP-AG8CFCL>
2024-09-03 15:30:19 -0300

    Agregar archivos de proyecto.

---
+++ ProyectConnectApiUseDolarApi/Controllers/DolarController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectConnectApiUseDolarApi.Models;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace ProyectConnectApiUseDolarApi.Controllers
+{
+    public class DolarController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public DolarController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://dolarapi.com");
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Inicio()
+        {
+            var respuesta = await _httpClient.GetAsync("/v1/dolares");
+            if(respuesta.IsSuccessStatusCode)
+            {
+                var dolar = await respuesta.Content.ReadAsAsync<List<Dolar>>();
+                return View(dolar);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+    }
+}
---
+++ ProyectConnectApiUseDolarApi/Models/Dolar.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace ProyectConnectApiUseDolarApi.Models
+{
+    public class Dolar
+    {
+        public float Compra { get; set; }
+        public float Venta { get; set; }
+        public string Casa { get; set; } = null!;
+        public string Nombre { get; set; } = null!;
+        public string Moneda { get; set; } = null!;
+        [DisplayName("Fecha de Actualizacion")]
+        public string FechaActualizacion { get; set; } = null!;
+    }
+}
